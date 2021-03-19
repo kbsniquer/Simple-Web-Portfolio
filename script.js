01ae308b3684e87a03a9f1c4246680499eafc065
kbsniquer <65562500+kbsniquer@users.noreply.github.com>
2021-03-19 16:21:30 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Adjust the scrolling offset
-function scrollToTargetAdjusted(id){
-document.getElementById(id).scrollIntoView(false)
-};
+function scrollToTargetAdjusted(id) {
+  document.getElementById(id).scrollIntoView(false);
+}
 
+$(window).scroll(function () {
+  $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250);
+});
